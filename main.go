dd1c14189166ca6390b46e89c964bc1490fea6e9
yusukemisa <yusukemisawa2014@gmail.com>
2018-05-19 18:14:27 +0900

    [modify] 構造体追加

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
 
 	"google.golang.org/appengine"
 )
+
+type gomikuji struct {
+	t time.Time
+}
+
+type omikujiResult struct {
+	Fortune  string `json:"fortune"`
+	ImageURL string `json:"imageURL"`
+}
 
 func main() {
 	http.HandleFunc("/", handle)
@@ -16,25 +28,56 @@ func main() {
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, omikuji())
+	g := &gomikuji{
+		t: time.Now(),
+	}
+	result := g.omikuji()
+
+	jsonString, err := result2JsonString(result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintln(w, jsonString)
 }
 
-func omikuji() string {
-	castNum := rand.Intn(6) + 1
-	var result string
+//おみくじ結果をJOSN文字列に変換
+func result2JsonString(o *omikujiResult) (string, error) {
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(o); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+//おみくじロジック
+func (g *gomikuji) omikuji() *omikujiResult {
+	castNum := g.cast()
+	var result, imgURL string
 	switch castNum {
 	case 1:
 		result = "凶"
-	case 2:
+		imgURL = ""
+	case 2, 3:
 		result = "吉"
-	case 3:
-		result = "吉"
-	case 4:
+		imgURL = ""
+	case 4, 5:
 		result = "中吉"
-	case 5:
-		result = "中吉"
+		imgURL = ""
 	case 6:
 		result = "大吉"
+		imgURL = ""
 	}
-	return fmt.Sprintf("おみくじ結果：%v,さいのめ：%v\n", result, castNum)
+	return &omikujiResult{
+		Fortune:  result,
+		ImageURL: imgURL,
+	}
+
+}
+
+func (g *gomikuji) cast() int {
+	//日にちを取得
+	//_, m, d := g.t.Date()
+	//if m == time.January &&
+	return rand.Intn(6) + 1
 }
